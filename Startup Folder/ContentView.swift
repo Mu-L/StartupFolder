3fc5333acb3fdd8e9cc7811700594f5b423264a4
Alin Panaitiu <alin.panaitiu@gmail.com>
2025-01-16 16:36:45 +0200

    Initial Commit

---
+++ Startup Folder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Startup Folder
+//
+//  Created by Alin Panaitiu on 16.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Startup Folder/Startup_FolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Startup_FolderApp.swift
+//  Startup Folder
+//
+//  Created by Alin Panaitiu on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct Startup_FolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
